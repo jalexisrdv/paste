45d72f08d47b61daee0eb4ee02fe4f903f6c52ec
JARV <alexisram98@outlook.com>
2020-01-07 11:46:47 -0600

    Datos de conexion modificados

--- models/connection.php
+++ models/connection.php
@@ -9,7 +9,7 @@ class Connection {
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
             ];
-            $connection = new PDO("mysql:host=localhost;dbname=mxzmampf_mnpst;", "mxzmampf_mpt", "L_wFFbZu@Q%o", $options);
+            $connection = new PDO("mysql:host=localhost;dbname=multi_paste;", "root", "root", $options);
             return $connection;
         }catch(PDOException $e) {
             echo 'Error durante la conexion: ' . $e->getMessage();
